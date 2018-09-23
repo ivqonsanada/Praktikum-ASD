9d5cbcca244cf702b9451e5eba23769914f56044
Im0z <hanyisfa@gmail.com>
2018-09-23 22:12:24 +0700

    Add files via upload

---
+++ Praktikum4/SingleLinkedList.java
@@ -0,0 +1,148 @@
+package Praktikum4;
+
+public class SingleLinkedList {
+
+    private Node head = null;
+    private Node tail = null;
+    private int size = 0;
+
+    public SingleLinkedList() {
+    }
+
+    public void sisipDidepan(Buku dt) {
+        Node nodeBaru = new Node();
+        nodeBaru.data = dt;
+        nodeBaru.next = head;
+        head = nodeBaru;
+    }
+
+    public boolean sisip(Buku dt1, Buku dt2) {
+        Node temp = head;
+        Node nodeBaru = new Node();
+        nodeBaru.data = dt1;
+        while (temp != null && temp.data != dt2) {
+            temp = temp.next;
+        }
+        if (temp != null) {
+            nodeBaru.next = temp.next;
+            temp.next = nodeBaru;
+            return true;
+        }
+        return false;
+    }
+
+    public Buku hapusDiDepan() {
+        if (head == tail) {
+            tail = null;
+        }
+        Node hapus = head;
+        head = head.next;
+        return hapus.data;
+    }
+
+    public Buku hapusDiAkhir() {
+        if (head == tail) {
+            tail = null;
+        } else {
+            Node temp = head;
+            while (temp.next != tail) {
+                temp = temp.next;
+            }
+            temp.next = null;
+            tail = temp;
+            temp = null;
+        }
+        return tail.data;
+    }
+
+    boolean isEmpty() {
+        return (size == 0);
+    }
+
+    public void sisipDataDiAkhir(Buku data) {
+        Node nodeBaru = new Node();
+        nodeBaru.data = data;
+        tail.next = nodeBaru;
+        tail = nodeBaru;
+    }
+
+    public void sisipDataUrut(Buku data) {
+        Node newNode = head;
+        Node temp = new Node();
+        temp.data = data;
+
+        if (head == null) {
+            head = tail = temp;
+        } else if (temp.data.getHargaSewa() < head.data.getHargaSewa()) {
+            while (newNode.next != null) {
+                System.out.println(temp.data.judul + temp.data.getHargaSewa());
+                System.out.println(newNode.data.judul + newNode.data.getHargaSewa());
+                if (temp.data.getHargaSewa() == newNode.data.getHargaSewa()) {
+                    System.out.println(temp.data.judul);
+                    while (newNode.next != null) {
+                        if (temp.data.getHargaDenda() < newNode.data.getHargaDenda()) {
+                            newNode = newNode.next;
+                        } else {
+                            break;
+                        }
+                    }
+                    break;
+                } else if (temp.data.getHargaSewa() < newNode.data.getHargaSewa()) {
+                    System.out.println("bener dak sih");
+                    newNode = newNode.next;
+                } else {
+                    System.out.println("juasik");
+                    break;
+                }
+            }
+            temp.next = newNode.next;
+            newNode.next = temp;
+        }
+        else {
+            temp.next = head;
+            head = temp;
+        }
+    }
+
+    public void hapusData(Buku key) {
+        Node temp = head;
+        if (!isEmpty()) {
+            while (temp != null) {
+                if (temp.next.data.getHargaDenda() == key.getHargaDenda()) {
+                    temp.next = temp.next.next;
+                    if (temp.next == null) {
+                        tail = temp;
+                    }
+                    break;
+                } else if ((temp.data.getHargaDenda() == key.getHargaDenda()) && (temp == head)) {
+                    this.hapusDiDepan();
+                    break;
+                }
+                temp = temp.next;
+            }
+        } else {
+            System.out.println("Data is empty!");
+        }
+        size--;
+    }
+
+    public Node getPointer() {
+        return head;
+    }
+
+    public void cetak(String kom) {
+        System.out.println(kom);
+        Node n = head;
+        while (n != null) {
+            System.out.println("==================");
+            System.out.println("JUDUL       :" + n.data.getJudul());
+            System.out.println("KATEGORI    :" + n.data.getKategori());
+            System.out.println("HARGA SEWA  :" + n.data.getHargaSewa());
+            System.out.println("HARGA DENDA :" + n.data.getHargaDenda());
+            System.out.println("==================");
+            n = n.next;
+        }
+        System.out.println("NULL");
+    }
+
+}
---
+++ Praktikum4/main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum4;
+
+/**
+ *
+ * @author rockwell
+ */
+public class main {
+
+    public static void main(String[] args) {
+        Buku buku1 = new Buku("Komputer", "Pemrograman Java", 2000, 3000);
+        Buku buku2 = new Buku("Pendidikan", "Matematika SMA", 2500, 2500);
+        Buku buku3 = new Buku("Komputer", "Belajar Microsoft Office", 2000, 7000);
+        Buku buku4 = new Buku("Novel", "Laskar Pelangi", 1000, 4000);
+        Buku buku5 = new Buku("Komik", "Doraemon", 2000, 5000);
+        SingleLinkedList ll = new SingleLinkedList();
+        ll.sisipDataUrut(buku1);
+        ll.sisipDataUrut(buku2);
+        ll.sisipDataUrut(buku3);
+        ll.sisipDataUrut(buku4);
+        ll.sisipDataUrut(buku5);
+        ll.cetak("LINKED LIST BUKU");
+    }
+}
