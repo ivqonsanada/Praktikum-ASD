5c27082b884cddc751fe6100f9afe53fd08f1f59
Ivqonnada Al Mufarrih <36329890+ivqonsanada@users.noreply.github.com>
2018-11-27 14:06:45 +0700

    Create buku.java

---
+++ Praktikum7/buku.java
@@ -0,0 +1,13 @@
+package praktikum7;
+public class Buku {
+    public Buku(String Judul) {
+        this.Judul = Judul;
+    }
+    private String Judul;
+    public String getJudul() {
+        return Judul;
+    }
+    public void setJudul(String Judul) {
+        this.Judul = Judul;
+    }
+}
