95292842b5f434493ea8655c7ba4be83f511315a
Ivqonnada Al Mufarrih <36329890+ivqonsanada@users.noreply.github.com>
2018-11-27 14:06:12 +0700

    Update main.java

--- Praktikum7/main.java
+++ Praktikum7/main.java
@@ -1,20 +1,15 @@
-package Praktikum7;
-
+package praktikum7;
 public class main {
-
     public static void main(String[] args) {
         Stack st = new Stack();
         st.push(0);
         st.push(6);
         st.push(7);
         st.cetak("Awal");
-
         System.out.println(st.peek());
-
         while (!st.isEmpty()) {
             System.out.println(st.pop());
         }
-//        String pos = st.infixToPostfix("2+3*4+5/(3+7)");
         String pos = st.infixToPostfix("2+3*4-6/2");
         System.out.println(pos);
         String pre = st.infixToPrefix("2+3*4+5/(3+7)");
@@ -22,5 +17,21 @@ public class main {
         int inpos = st.countIn2Post(pos);
         System.out.println("================");
         System.out.println(inpos);
+        //implementasi Stack
+        Stack std = new Stack();
+        st.push(new Double(5));
+        st.push(new Double(8));
+        st.push(new Double(7));
+        while (!st.isEmpty()) {
+            System.out.println(st.pop());
+        }
+        Stack stBuku = new Stack();
+        stBuku.push(new Buku("Java"));
+        stBuku.push(new Buku("Algoritma dan STD"));
+        stBuku.push(new Buku("C++"));
+        System.out.println(stBuku.peek());
+        while (!stBuku.isEmpty()) {
+            System.out.println(stBuku.pop());
+        }
     }
 }
